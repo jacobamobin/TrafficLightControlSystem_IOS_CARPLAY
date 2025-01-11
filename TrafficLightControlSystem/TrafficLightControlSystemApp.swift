4bde954ae64c5be23a296485dd84d3ef16be0114
Jacob Mobin <jacobamobin@gmail.com>
2025-01-10 19:29:32 -0500

    Initial Commit

---
+++ TrafficLightControlSystem/TrafficLightControlSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightControlSystemApp.swift
+//  TrafficLightControlSystem
+//
+//  Created by Jacob Mobin on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightControlSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
